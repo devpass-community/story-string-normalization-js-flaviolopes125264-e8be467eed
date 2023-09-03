f7adba5262db7e87d332f33572b99c30084ed25d
FlavioAraujo23 <flaviolopes1027@gmail.com>
2023-09-03 11:50:46 -0300

    First commit

--- removeAccent.js
+++ removeAccent.js
@@ -1,5 +1,6 @@
-function removeAccent(str) {
-    // Add your solution here!
-}
+function removeAccent(text) {
+    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  }
+  // Esse código usa o método normalize com o argumento "NFD", que separa as letras acentuadas em caracteres individuais. Em seguida, a expressão regular /[\u0300-\u036f]/g é usada para remover esses caracteres.
 
 module.exports = removeAccent;
